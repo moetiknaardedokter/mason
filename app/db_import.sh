a101126abeba7061a06f75433b2d70cba577a58b
Janw Oostendorp <webmaster@janw.me>
2022-06-10 17:03:43 +0200

    formatting errors continue.

--- app/db_import.sh
+++ app/db_import.sh
@@ -2,7 +2,7 @@
 
 function db_import() {
   echo 'Dumping DB'
-  PREFIX=wp @live config get table_prefix --type=variable
+  PREFIX=$(wp @live config get table_prefix --type=variable)
   wp @live db dump --exclude_tables=${PREFIX}toolset_maps_address_cache - > dump.sql
   #------------------------------------------------------------------------^ Formatting will strip this space, keep it!!
   echo 'Importing DB'
