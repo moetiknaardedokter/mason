195b647f09a0db8f42cf708bd7fa7dd6cdce2896
Janw Oostendorp <webmaster@janw.me>
2022-09-28 14:17:20 +0200

    Fixed dynamic sync path. for uploads.

--- app/sync_uploads.sh
+++ app/sync_uploads.sh
@@ -3,11 +3,14 @@
 function sync_uploads() {
   LIVE_SSH_SERVER=$(echo ${LIVE_SSH} | awk -F: '{print $1}')
   LIVE_SSH_PORT=$(echo ${LIVE_SSH} | awk -F: '{print $2}')
-  PORT=()
+  PORT=''
   if [[ ! -z "${LIVE_SSH_PORT}" ]]; then
     # @link https://superuser.com/a/360986
-    PORT=(-e "ssh -p ${LIVE_SSH_PORT}")
+    PORT="-e 'ssh -p ${LIVE_SSH_PORT}'"
   fi
 
-  rsync "${PORT[@]}" ${LIVE_SSH_SERVER}:${LIVE_PATH}wp-content/uploads/ ${LOCAL_PATH}wp-content/uploads/ -Pqavz
+  LIVE_UPLOADS_DIR=$(wp @live eval 'echo wp_get_upload_dir()["basedir"];')
+  LOCAL_UPLOADS_DIR=$(wp eval 'echo wp_get_upload_dir()["basedir"];')
+
+  eval "rsync ${PORT} ${LIVE_SSH_SERVER}:${LIVE_UPLOADS_DIR%/}/ ${LOCAL_UPLOADS_DIR/}/ ${EXCLUDES} --delete -Pqavz"
 }
